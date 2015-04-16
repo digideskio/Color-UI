1b0367c2b43f4666356010eac3ef3e8502e82598
Jacob Hash <jhash@umich.edu>
2015-04-16 17:25:04 -0400

    Updated with latest - before rehearsal

--- color_ui.lua
+++ color_ui.lua
@@ -1,22 +1,40 @@
+--Team Members--
+--Avi Mattenson, Eliana Abraham, Jiawei Wang, Jacob Hash, Longhan Li, Yuxuan Zhou--
+
 redOffset = 0
 greenOffset = 0
 blueOffset = 0
+red = 0
+blue = 0
+green = 0
+yversion = 0
 mode = 0
 
 --audio patterns
 
 ucPush1 = FlowBox("object","PushA1", _G["FBPush"])
 ucPush2 = FlowBox("object","PushA2", _G["FBPush"])
+ucPush3 = _G["FBCam"]
+ucPush4 = _G["FBGain"]
 dac = _G["FBDac"]
 
 --if not ucSample then
 	ucSample = FlowBox("object","Sample", _G["FBSample"])
-	ucSample:AddFile("piano3.wav")
-	ucSample:AddFile("violin3.wav")
-	ucSample:AddFile("drum3.wav")
+	ucSample:AddFile("green_light_sounds_converted.wav")
+	ucSample:AddFile("yellow_light_sounds_converted.wav")
+	ucSample:AddFile("yellow_light_sounds_converted1.wav")
+	ucSample:AddFile("red_light_sounds_converted.wav")
+	ucSample:AddFile("Godzilla_converted1.wav")
 --end
 
-dac:SetPullLink(0, ucSample, 0)
+
+ucPush4.Amp:SetPull(ucPush3.Bright)
+
+ucPush4:SetPullLink(0, ucSample, 0)
+dac.In:SetPull(ucPush4.Out)
+ucPush4:Push(1)
+
+--dac:SetPullLink(0, ucSample, 0)
 
 ucPush1:SetPushLink(0,ucSample, 3)
 --ucPush1:Push(0)
@@ -25,9 +43,9 @@ ucPush2:Push(1.0)
 
 -- color methods
 
-RED, GREEN, YELLOW = 0, 1, 2
+RED, GREEN, YELLOW, BLUE, NONE = 0, 1, 2, 3, 4
 
-currentColor = RED
+currentColor = NONE
 
 function isRed(r, g, b)
 	local RED_DIFFERENCE = 100
@@ -40,21 +58,28 @@ function isGreen(r, g, b)
 end
 
 function isYellow(r, g, b)
-	local YELLOW_MAX_B = 160
+	local YELLOW_MAX_B = 80
 	local YELLOW_MIN_RG = 120
 	return (r + g) / 2 > YELLOW_MIN_RG and b < YELLOW_MAX_B and math.abs(r - g) < 50
+end
+
+function isBlue(r, g, b)
+	local BLUE_DIFFERENCE = 100
+	return g + 50 < b and r + 150 < b
 end
 
 function playAudio(color)
 	ucPush2:Push(1.0)
 	ucPush2:Push(0.0)
-	DPrint("ChangeAudio")
 	if color == YELLOW then
-		ucPush1:Push(0/2)
+		num = 1+yversion
+		ucPush1:Push(num/4)
 	elseif color == GREEN then
-		ucPush1:Push(1/2)
+		ucPush1:Push(0/4)
 	elseif color == RED then
-		ucPush1:Push(2/2)
+		ucPush1:Push(3/4)
+	elseif color == BLUE then
+		ucPush1:Push(4/4)
 	end
 end
 
@@ -69,29 +94,35 @@ end
  	elseif isRed(r,g,b) then
  		DPrint("Red")
  		currentColor = RED
- 	end
+ 	elseif isBlue(r,g,b) then
+		DPrint("Blue")
+		currentColor = BLUE
+	else
+		DPrint("nocolor")
+	end
+
 	if lastColor ~= currentColor then
 			playAudio(currentColor)
 	end
- 	-- TODO: Longhan - call function to play sound based on color here
  end
 
 function updateHandler(self, elapsedTime)
-	r1,g1,b1 = camera.t:PixelColor(ScreenWidth()/6, ScreenHeight()/6)
-	r2,g2,b2 = camera.t:PixelColor(ScreenWidth()/6, ScreenHeight()/12)
-	r3,g3,b3 = camera.t:PixelColor(ScreenWidth()/6, ScreenHeight()/4)
+	if frozen == 1 then
+		return
+	end
+
+	r1,g1,b1 = camera.t:PixelColor(ScreenWidth()/2, ScreenHeight()/2)
+	r2,g2,b2 = camera.t:PixelColor(ScreenWidth()/2, ScreenHeight()/4)
+	r3,g3,b3 = camera.t:PixelColor(ScreenWidth()/2, ScreenHeight()*3/4)
 
 	r4 = math.floor((r1+r2+r3)/3)
 	g4 = math.floor((g1+g2+g3)/3)
 	b4 = math.floor((b1+b2+b3)/3)
 
-	if mode == 0 and frozen == 0 then
+	--r4 = ucPush3.Red
+	if mode == 0 then
 		-- Nothing needed
-	elseif mode == 2 and frozen == 0 then
-		r4 = redOffset
-		g4 = greenOffset
-		b4 = blueOffset
-	elseif mode == 1 and frozen == 0 then
+	elseif mode == 1 then
 		r4 = r4 + redOffset
 		g4 = g4 + greenOffset
 		b4 = b4 + blueOffset
@@ -127,60 +158,58 @@ function freezerHandler(self)
 	end
 end
 
-function setCam(self)
+function setCam(self, texture)
+	settingButtons[0].t:SetSolidColor(255,153,153,255)
+	settingButtons[1].t:SetSolidColor(128,128,128,255)
 	mode = 0
 	board:Hide()
 end
 
 function setCamMan(self)
+	settingButtons[0].t:SetSolidColor(128, 128, 128, 255)
+	settingButtons[1].t:SetSolidColor(255,153,153,255)
 	mode = 1
 	board:Hide()
 end
 
-function setMan(self)
-	mode = 2
-	board.t:SetSolidColor(redOffset, greenOffset, blueOffset, 255)
-	board:Show()
-end
-
 function addRed(self)
 	if redOffset < 255 then
-		redOffset = redOffset + 1
+		redOffset = redOffset + 10
 		infoBoxes0[0].title:SetLabel("Offset:" .. redOffset)
 	end
 end
 
 function minusRed(self)
 	if redOffset > -255 then
-		redOffset = redOffset - 1
+		redOffset = redOffset - 10
 		infoBoxes0[0].title:SetLabel("Offset:" .. redOffset)
 	end
 end
 
 function addGreen(self)
 	if greenOffset < 255 then
-		greenOffset = greenOffset + 1
+		greenOffset = greenOffset + 10
 		infoBoxes0[1].title:SetLabel("Offset:" .. greenOffset)
 	end
 end
 
 function minusGreen(self)
 	if greenOffset > -255 then
-		greenOffset = greenOffset - 1
+		greenOffset = greenOffset - 10
 		infoBoxes0[1].title:SetLabel("Offset:" .. greenOffset)
 	end
 end
 
 function addBlue(self)
 	if blueOffset < 255 then
-		blueOffset = blueOffset + 1
+		blueOffset = blueOffset + 10
 		infoBoxes0[2].title:SetLabel("Offset:" .. blueOffset)
 	end
 end
 
 function minusBlue(self)
 	if blueOffset > -255 then
-		blueOffset = blueOffset - 1
+		blueOffset = blueOffset - 10
 		infoBoxes0[2].title:SetLabel("Offset:" .. blueOffset)
 	end
 end
@@ -189,6 +218,12 @@ function resetHandler(self)
 	redOffset = 0
 	greenOffset = 0
 	blueOffset = 0
+	red = 0
+	green = 0
+	blue = 0
+	reset = 1
+	currentColor = NULL
+	ucPush2:Push(1)
 	infoBoxes0[0].title:SetLabel("Offset:" .. redOffset)
 	infoBoxes0[1].title:SetLabel("Offset:" .. greenOffset)
 	infoBoxes0[2].title:SetLabel("Offset:" .. blueOffset)
@@ -200,15 +235,41 @@ function changePage1(self, x, y, dx, dy)
 	end
 end
 
-function Page2Info(self)
-	texture = self:Texture()
-	texture:SetSolidColor(255, 0, 0, 255)
+function Page2InfoRed1(self)
+	--buttons[0].t = buttons[0]:Texture()
+	buttons[0].t:SetSolidColor(255, 0, 0, 255)
+	buttons[3].t:SetSolidColor(255, 255, 255, 255)
 end
 
-function Page2Clear(self)
-	texture = self:Texture()
-	texture:SetSolidColor(255,255,255,255)
+function Page2InfoYellow1(self)
+	buttons[1].t:SetSolidColor(255, 0, 0, 255)
+	buttons[4].t:SetSolidColor(255, 255, 255, 255)
+	yversion = 0
 end
+
+function Page2InfoGreen1(self)
+	buttons[2].t:SetSolidColor(255, 0, 0, 255)
+	buttons[5].t:SetSolidColor(255, 255, 255, 255)
+end
+
+function Page2InfoRed2(self)
+	--buttons[3].t = buttons[3]:Texture()
+	buttons[3].t:SetSolidColor(255, 0, 0, 255)
+	buttons[0].t:SetSolidColor(255, 255, 255, 255)
+end
+
+function Page2InfoYellow2(self)
+	buttons[4].t:SetSolidColor(255, 0, 0, 255)
+	buttons[1].t:SetSolidColor(255, 255, 255, 255)
+	yversion = 1
+end
+
+function Page2InfoGreen2(self)
+	buttons[5].t:SetSolidColor(255, 0, 0, 255)
+	buttons[2].t:SetSolidColor(255, 255, 255, 255)
+end
+
+
 
 function changePage2(self, x, y, dx, dy)
 	if dx < -10 then
@@ -249,8 +310,8 @@ color:SetHeight(ScreenHeight()/3)
 color:SetAnchor("LEFT", camera, "RIGHT", ScreenWidth()/3, 0)
 color.t = color:Texture(0, 0, 0, 255)
 color.title = color:TextLabel()
-color.title:SetFontHeight(20)
-color.title:SetLabel("Major Camera Color")
+color.title:SetFontHeight(15)
+color.title:SetLabel("Major Camera Color: ")
 color.title:SetVerticalAlign("TOP")
 color.title:SetColor(255, 255, 0, 255)
 color:Show()
@@ -285,12 +346,12 @@ reset:Handle("OnTouchDown", resetHandler)
 reset:EnableInput(true)
 
 settingButtons = {}
-for i= 0, 2 do
+for i= 0, 1 do
 	settingButtons[i] = Region()
 	settingButtons[i]:SetWidth(ScreenWidth()/4)
 	settingButtons[i]:SetHeight(ScreenHeight()/8)
 	settingButtons[i]:SetLayer("TOP")
-	settingButtons[i]:SetAnchor("TOPLEFT", r, "TOPLEFT", (i+1)*ScreenWidth()/16+i*ScreenWidth()/4, -ScreenHeight()*1/32)
+	settingButtons[i]:SetAnchor("TOPLEFT", r, "TOPLEFT", (i+1)*ScreenWidth()/6+i*ScreenWidth()/4, -ScreenHeight()*1/32)
 	settingButtons[i].t = settingButtons[i]:Texture(255, 0, 0, 255)
 	settingButtons[i]:Show()
 	settingButtons[i].title = settingButtons[i]:TextLabel()
@@ -300,13 +361,10 @@ for i= 0, 2 do
 end
 settingButtons[0].title:SetLabel("Camera")
 settingButtons[1].title:SetLabel("Camera+Manual")
-settingButtons[2].title:SetLabel("Manual")
-settingButtons[0].t:SetSolidColor(0, 255, 0, 255)
-settingButtons[1].t:SetSolidColor(255, 255, 0, 255)
-settingButtons[2].t:SetSolidColor(255, 0, 0, 255)
+settingButtons[0].t:SetSolidColor(128, 128, 128, 255)
+settingButtons[1].t:SetSolidColor(128, 128, 128, 255)
 settingButtons[0]:Handle("OnTouchDown", setCam)
 settingButtons[1]:Handle("OnTouchDown", setCamMan)
-settingButtons[2]:Handle("OnTouchDown", setMan)
 
 texts = {}
 for i=0,2 do
@@ -314,7 +372,7 @@ for i=0,2 do
 	texts[i]:SetWidth(ScreenWidth()/4)
 	texts[i]:SetHeight(ScreenHeight()/12)
 	texts[i]:SetLayer("LOW")
-	texts[i]:SetAnchor("TOP", settingButtons[i], "BOTTOM", 0, -ScreenHeight()*1/256)
+	texts[i]:SetAnchor("TOPLEFT", r, "TOPLEFT", (i+1)*ScreenWidth()/16+i*ScreenWidth()/4, -ScreenHeight()*1/6)
 	texts[i].t = texts[i]:Texture(255, 255, 255, 255)
 	texts[i]:Show()
 	texts[i].title = texts[i]:TextLabel()
@@ -433,15 +491,22 @@ for i=0, 5 do
 	buttons[i].title:SetColor(0, 0, 0, 255)
 	buttons[i].title:SetFontHeight(35)
 	buttons[i]:Handle("OnTouchDown", Page2Info)
-	buttons[i]:Handle("OnDoubleTap", Page2Clear)
 	buttons[i]:EnableInput(true)
 	buttons[i]:Show()
 end
-buttons[0].title:SetLabel("Piano")
-buttons[1].title:SetLabel("Violin")
-buttons[2].title:SetLabel("Drum")
-buttons[3].title:SetLabel("Flute")
-buttons[4].title:SetLabel("Guitar")
-buttons[5].title:SetLabel("Saxphone")
-		
+
+buttons[0]:Handle("OnTouchDown", Page2InfoRed1)
+buttons[1]:Handle("OnTouchDown", Page2InfoYellow1)
+buttons[2]:Handle("OnTouchDown", Page2InfoGreen1)
+buttons[3]:Handle("OnTouchDown", Page2InfoRed2)
+buttons[4]:Handle("OnTouchDown", Page2InfoYellow2)
+buttons[5]:Handle("OnTouchDown", Page2InfoGreen2)
+
+buttons[0].title:SetLabel("Red V.1")
+buttons[1].title:SetLabel("Yellow V.1")
+buttons[2].title:SetLabel("Green V.1")
+buttons[3].title:SetLabel("Red V.2")
+buttons[4].title:SetLabel("Yellow V.2")
+buttons[5].title:SetLabel("Green V.2")
+
 SetPage(1)
